822df758fdce7348944927cee6aed44aa31f089a
Dhaval Patel <erdhavalpatel@yahoo.com>
2021-05-08 11:53:39 +0530

    intial check in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+  Badge,
+} from "reactstrap";
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [intervalContext] = useState({ interval: null });
+  const [pincode, setPincode] = useState("");
+  const [intervalSec, setIntervalinSec] = useState(30000);
+  const [invalid, setInvalid] = useState(false);
+  const fetchDate = () => {
+    const getDate = new Date();
+    const preparedDate = `${getDate.getDate()}-${
+      getDate.getMonth() + 1
+    }-${getDate.getFullYear()}`;
+    fetch(
+      `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=390013&date=${preparedDate}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const centers = res.centers;
+        if (centers) {
+          centers.map(({ address, sessions }) => {
+            if (address === "MAA GAYATRI SCHOOL MANEJA GAM RAOD") {
+              sessions.map(({ available_capacity, min_age_limit }) => {
+                console.log(min_age_limit);
+                setCount(available_capacity);
+              });
+            }
+          });
+        }
+      });
+  };
+  useEffect(() => {}, []);
+  const startImterval = () => {
+    if (pincode) {
+      intervalContext.interval = setInterval(fetchDate, intervalSec);
+      setInvalid(false);
+    } else {
+      setInvalid(true);
+      toast.error("Enter pincode please!");
+    }
+  };
+  const stopInterval = () => {
+    if (intervalContext.interval) {
+      clearInterval(intervalContext.interval);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ToastContainer />
+      <Row className="m-5" xs="3">
+        <FormGroup>
+          <Label for="exampleEmail">Area Code</Label>
+          <Input
+            placeholder="e.g 390013"
+            invalid={invalid}
+            onChange={(e) => setPincode(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">
+            Interval in millisecond(default 30 second)
+          </Label>
+          <Input
+            placeholder="1000 ms = 1 second"
+            onChange={(e) => setIntervalinSec(e.target.value)}
+          />
+        </FormGroup>
+      </Row>
+      <Row xs="3" className="m-5">
+        <FormGroup>
+          <Button color="primary" onClick={startImterval}>
+            Start Interval
+          </Button>
+          <Button color="danger" className="m-2" onClick={stopInterval}>
+            Clear Interval
+          </Button>
+        </FormGroup>
+      </Row>
+      <Row xs="3">
+        <h1>Total Vaccine Available</h1>
+        <Row>
+          <h1>{count}</h1>
+        </Row>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
